a330a6b332b3e8136e32d024e440ad44298520e0
Jason M <jbird206@gmail.com>
2020-01-21 10:45:28 -0800

    Add files via upload
    created a loop

--- array-practice/js/scripts.js
+++ array-practice/js/scripts.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
   var foods = [];
-  console.log(foods);
 
 
   $("form").submit(function(event) {
+    $(".printed").remove();
+
     var currentInput = $("#foodInput").val();
     foods.push(currentInput);
-    var food1 = foods[0];
-    var food2 = foods[1];
-    var food3 = foods[2];
 
-    $(".results1").text(food1);
-    if (food2) {
-      $(".results2").text(food2);
-    }
-    if (food3) {
-      $(".results3").text(food3);
-    }
+    foods.forEach(function(food){
+      $(".results").append("<p class='printed'>" + food + "</p>");
+    });
+
+    
+
+
+  
 
     event.preventDefault();
     
